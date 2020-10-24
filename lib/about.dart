719744cdd55b7aa0378661fbc4bc812d46e216ca
jeffubayi <jeffubayi@gmail.com>
2020-10-24 12:01:01 +0300

    add about page gif

--- lib/about.dart
+++ lib/about.dart
@@ -1,27 +1,22 @@
 import 'package:flutter/material.dart';
 
 class About extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("About Beba"),
-              backgroundColor: Colors.grey[850]),
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            SizedBox(height: 50.0),
-            Column(
-              children: <Widget>[
-                Image.asset("Carpool.gif"),
-                SizedBox(height: 125),
-              ],
-            )
-           ] 
-           )
-           
-      )
-    );
+        appBar: AppBar(
+            title: Text("About Beba"), backgroundColor: Colors.grey[850]),
+        body: SafeArea(
+            child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                children: <Widget>[
+              SizedBox(height: 50.0),
+              Column(
+                children: <Widget>[
+                  Image.asset("assets/images/Carpool.gif"),
+                  SizedBox(height: 125),
+                ],
+              )
+            ])));
   }
 }
